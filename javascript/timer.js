4a37603633b4989264a85849c014c1c82037fa2c
adrian_rampy <ahrampy@gmail.com>
2021-03-12 12:18:11 -0800

    update icons

--- javascript/timer.js
+++ javascript/timer.js
@@ -1,6 +1,6 @@
 const storage = window.localStorage;
 
-const dom = {
+const domObj = {
   body: null,
   form: null,
   timerInput: null,
@@ -10,22 +10,42 @@ const dom = {
   addBtn: null,
   audioBtn: null,
   audioIcon: null,
+  bgBtn: null,
+  bgIcon: null,
+  bgOptions: null,
 };
 
-const global = {
-  // time
+const timeObj = {
+  // hours, minutes, seconds
+  time: [0, 0, 0],
+
   display: null,
   duration: null,
   start: null,
   diff: null,
-  // hours, minutes, seconds
-  time: [0, 0, 0],
-
-  // internal
   interval: null,
 };
 
-const assets = {
+const themeObj = {
+  theme: "river",
+  river: {
+    img: 1,
+    main: "#e8f8f8",
+    accent: "#1b3532",
+  },
+  mountain: {
+    img: 2,
+    main: "#fff",
+    accent: "#333",
+  },
+  window: {
+    img: 3,
+    main: "#333",
+    accent: "#fff",
+  },
+};
+
+const assetsObj = {
   ding: null,
 };
 
@@ -33,98 +53,17 @@ const checkStorage = () => {
   if (storage.getItem("volume") === undefined) {
     storage.setItem("volume", "on");
   }
+  if (storage.getItem("theme") === undefined) {
+    storage.setItem("theme", "river");
+  }
   updateAudio();
-};
-
-const timerKickoff = (mins = 0, secs = 0) => {
-  global.duration = mins * 60 + secs * 6;
-  global.start = Date.now();
-  timer();
-  global.interval = setInterval(timer, 1000);
-};
-
-function timer() {
-  global.diff = global.duration - (((Date.now() - global.start) / 1000) | 0);
-
-  global.time[0] = Math.floor(global.diff / 3600) | 0;
-  global.time[1] = Math.floor((global.diff % 3600) / 60) | 0;
-  global.time[2] = Math.floor((global.diff % 3600) % 60) | 0;
-
-  global.time[0] = global.time[0] < 10 ? "0" + global.time[0] : global.time[0];
-  global.time[1] = global.time[1] < 10 ? "0" + global.time[1] : global.time[1];
-  global.time[2] = global.time[2] < 10 ? "0" + global.time[2] : global.time[2];
-
-  if (global.diff <= 60) dom.subtractBtn.style.opacity = 0;
-  if (global.diff === 0) {
-    if (storage.getItem("volume") === "on") assets.ding.play();
-    dom.subtractBtn.style.opacity = "0";
-    dom.addBtn.style.opacity = "0";
-    dom.clearBtn.innerHTML = "new";
-    clearInterval(global.interval);
-  } else if (global.diff < 0) return;
-
-  changeDisplay();
-}
-
-const changeDisplay = () => {
-  if (global.time[0] === "00" || global.time[0] <= 0) {
-    global.display = global.time[1] + ":" + global.time[2];
-  } else {
-    global.display =
-      global.time[0] + ":" + global.time[1] + ":" + global.time[2];
-  }
-
-  dom.timerDiv.innerHTML = global.display;
-};
-
-const startTimer = (e) => {
-  e.preventDefault();
-
-  // select for any combo of valid minutes or minutes fractions by decimals
-  let regex = /^(-?\d+)*\.?([1-9])?$/;
-
-  let match = dom.timerInput.value.match(regex);
-  if (match) {
-    dom.timerInput.placeholder = "";
-    dom.timerInput.style.display = "none";
-    dom.timerDiv.style.display = "block";
-    dom.subtractBtn.style.opacity = "100";
-    dom.clearBtn.style.opacity = "100";
-    dom.addBtn.style.opacity = "100";
-    timerKickoff(match[1], match[2]);
-  }
-  dom.timerInput.value = "";
-};
-
-const removeTime = () => {
-  if (global.duration > 60) {
-    global.duration -= 60;
-    timer();
-  }
-};
-
-const addTime = () => {
-  global.duration += 60;
-  if (global.duration > 60) dom.subtractBtn.style.opacity = 100;
-  timer();
-};
-
-const stopTimer = () => {
-  clearInterval(global.interval);
-  dom.timerInput.style.display = "block";
-  dom.timerDiv.style.display = "none";
-  dom.subtractBtn.style.opacity = "0";
-  dom.clearBtn.innerHTML = "clear";
-  dom.clearBtn.style.opacity = "0";
-  dom.addBtn.style.opacity = "0";
-  dom.timerInput.focus();
 };
 
 const updateAudio = () => {
   if (storage.getItem("volume") === "on") {
-    dom.audioImg.src = "images/audio.png";
+    domObj.audioImg.src = "images/volume-on.png";
   } else {
-    dom.audioImg.src = "images/no-audio.png";
+    domObj.audioImg.src = "images/volume-off.png";
   }
 };
 
@@ -137,28 +76,129 @@ const toggleAudio = () => {
   updateAudio();
 };
 
+const toggleBgOptions = () => {
+  if (domObj.bgOptions.classList.contains("hidden")) {
+    domObj.bgOptions.classList.remove("hidden");
+    domObj.bgIcon.src = "../images/arrow-right.png";
+  } else {
+    domObj.bgOptions.classList.add("hidden");
+    domObj.bgIcon.src = "../images/arrow-left.png";
+  }
+};
+
+const timerKickoff = (mins = 0, secs = 0) => {
+  timeObj.duration = mins * 60 + secs * 6;
+  timeObj.start = Date.now();
+  timer();
+  timeObj.interval = setInterval(timer, 1000);
+};
+
+function timer() {
+  timeObj.diff = timeObj.duration - (((Date.now() - timeObj.start) / 1000) | 0);
+
+  timeObj.time[0] = Math.floor(timeObj.diff / 3600) | 0;
+  timeObj.time[1] = Math.floor((timeObj.diff % 3600) / 60) | 0;
+  timeObj.time[2] = Math.floor((timeObj.diff % 3600) % 60) | 0;
+
+  timeObj.time[0] =
+    timeObj.time[0] < 10 ? "0" + timeObj.time[0] : timeObj.time[0];
+  timeObj.time[1] =
+    timeObj.time[1] < 10 ? "0" + timeObj.time[1] : timeObj.time[1];
+  timeObj.time[2] =
+    timeObj.time[2] < 10 ? "0" + timeObj.time[2] : timeObj.time[2];
+
+  if (timeObj.diff <= 60) domObj.subtractBtn.style.opacity = 0;
+  if (timeObj.diff === 0) {
+    if (storage.getItem("volume") === "on") assetsObj.ding.play();
+    domObj.subtractBtn.style.opacity = "0";
+    domObj.addBtn.style.opacity = "0";
+    domObj.clearBtn.innerHTML = "new";
+    clearInterval(timeObj.interval);
+  } else if (timeObj.diff < 0) return;
+
+  changeDisplay();
+}
+
+const changeDisplay = () => {
+  if (timeObj.time[0] === "00" || timeObj.time[0] <= 0) {
+    timeObj.display = timeObj.time[1] + ":" + timeObj.time[2];
+  } else {
+    timeObj.display =
+      timeObj.time[0] + ":" + timeObj.time[1] + ":" + timeObj.time[2];
+  }
+
+  domObj.timerDiv.innerHTML = timeObj.display;
+};
+
+const startTimer = (e) => {
+  e.preventDefault();
+
+  // select for any int or a float with one decimal
+  let regex = /^(-?\d+)*\.?([1-9])?$/;
+
+  let match = domObj.timerInput.value.match(regex);
+  if (match) {
+    domObj.timerInput.placeholder = "";
+    domObj.timerInput.style.display = "none";
+    domObj.timerDiv.style.display = "block";
+    domObj.subtractBtn.style.opacity = "100";
+    domObj.clearBtn.style.opacity = "100";
+    domObj.addBtn.style.opacity = "100";
+    timerKickoff(match[1], match[2]);
+  }
+  domObj.timerInput.value = "";
+};
+
+const removeTime = () => {
+  if (timeObj.duration > 60) {
+    timeObj.duration -= 60;
+    timer();
+  }
+};
+
+const addTime = () => {
+  timeObj.duration += 60;
+  if (timeObj.duration > 60) domObj.subtractBtn.style.opacity = 100;
+  timer();
+};
+
+const stopTimer = () => {
+  clearInterval(timeObj.interval);
+  domObj.timerInput.style.display = "block";
+  domObj.timerDiv.style.display = "none";
+  domObj.subtractBtn.style.opacity = "0";
+  domObj.clearBtn.innerHTML = "clear";
+  domObj.clearBtn.style.opacity = "0";
+  domObj.addBtn.style.opacity = "0";
+  domObj.timerInput.focus();
+};
+
 const addDomEles = () => {
-  dom.body = document.querySelector("body");
-  dom.form = document.querySelector("#timerForm");
-  dom.timerInput = document.querySelector("#timerInput");
-  dom.timerDiv = document.querySelector("#timerClock");
-  dom.subtractBtn = document.querySelector("#subtract-time-btn");
-  dom.clearBtn = document.querySelector("#stop-timer");
-  dom.addBtn = document.querySelector("#add-time-btn");
-  dom.audioBtn = document.querySelector("#audio-btn");
-  dom.audioImg = document.querySelector("#audio-icon");
+  domObj.body = document.querySelector("body");
+  domObj.form = document.querySelector("#timerForm");
+  domObj.timerInput = document.querySelector("#timerInput");
+  domObj.timerDiv = document.querySelector("#timerClock");
+  domObj.subtractBtn = document.querySelector("#subtract-time-btn");
+  domObj.clearBtn = document.querySelector("#stop-timer");
+  domObj.addBtn = document.querySelector("#add-time-btn");
+  domObj.audioBtn = document.querySelector("#audio-btn");
+  domObj.audioImg = document.querySelector("#audio-icon");
+  domObj.bgBtn = document.querySelector("#bg-btn");
+  domObj.bgIcon = document.querySelector("#bg-icon");
+  domObj.bgOptions = document.querySelector(".bg-options");
 };
 
 const addListeners = () => {
-  dom.form.addEventListener("submit", startTimer);
-  dom.subtractBtn.addEventListener("click", removeTime);
-  dom.clearBtn.addEventListener("click", stopTimer);
-  dom.addBtn.addEventListener("click", addTime);
-  dom.audioBtn.addEventListener("click", toggleAudio);
+  domObj.form.addEventListener("submit", startTimer);
+  domObj.subtractBtn.addEventListener("click", removeTime);
+  domObj.clearBtn.addEventListener("click", stopTimer);
+  domObj.addBtn.addEventListener("click", addTime);
+  domObj.audioBtn.addEventListener("click", toggleAudio);
+  domObj.bgBtn.addEventListener("click", toggleBgOptions);
 };
 
 const loadAudio = () => {
-  assets.ding = new Audio("../audio/low-ding.mp3");
+  assetsObj.ding = new Audio("../audio/low-ding.mp3");
 };
 
 const init = () => {
@@ -166,7 +206,7 @@ const init = () => {
   addListeners();
   loadAudio();
   checkStorage();
-  dom.timerInput.focus();
+  domObj.timerInput.focus();
 };
 
 window.addEventListener("load", init);
